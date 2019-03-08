9c34f2c42b9d8ba34184d176a2de6760e84a9f96
ow <volovykolya@gmail.com>
2019-03-08 13:07:02 -0800

    add minor fixes to Models

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -14,10 +14,10 @@ namespace HairSalon.Models
       _id = id;
     }
 
-    public void SetName(string newName){_name = newName;}
-
     public string GetName(){return _name;}
     public int GetId(){return _id;}
+
+    public void SetName(string newName){_name = newName;}
 
     public static List<Stylist> GetAll()
     {
@@ -93,7 +93,7 @@ namespace HairSalon.Models
       MySqlConnection conn = DB.Connection();
       conn.Open();
       var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"DELETE FROM stylists WHERE id = @searchId; DELETE FROM clients WHERE stylist_id = @searchId;";
+      cmd.CommandText = @"DELETE FROM stylists WHERE id = @searchId; DELETE FROM clients WHERE stylist_id = @searchId; DELETE FROM stylists_specialties WHERE stylist_id = @searchId";
       MySqlParameter searchId = new MySqlParameter();
       searchId.ParameterName = "@searchId";
       searchId.Value = _id;
@@ -135,21 +135,55 @@ namespace HairSalon.Models
       return allStylistClients;
     }
 
-    public void DeleteClients()
+    public List<Specialty> GetSpecialties()
     {
       MySqlConnection conn = DB.Connection();
       conn.Open();
-      var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"DELETE FROM clients WHERE stylist_id = @searchId;";
-      MySqlParameter searchId = new MySqlParameter();
-      searchId.ParameterName = "@searchId";
-      searchId.Value = _id;
-      cmd.Parameters.Add(searchId);
-      cmd.ExecuteNonQuery();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT specialties.* FROM stylists
+      JOIN stylists_specialties ON (stylists.id = stylists_specialties.stylist_id)
+      JOIN specialties ON (stylists_specialties.specialty_id = specialties.id)
+      WHERE stylists.id = @stylistId;";
+      MySqlParameter stylistIdParameter = new MySqlParameter();
+      stylistIdParameter.ParameterName = "@stylistId";
+      stylistIdParameter.Value = _id;
+      cmd.Parameters.Add(stylistIdParameter);
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      List<Specialty> stylistSpecialties = new List<Specialty>{};
+      while(rdr.Read())
+      {
+        int specialtyId = rdr.GetInt32(0);
+        string specialtyName = rdr.GetString(1);
+        Specialty newSpecialty = new Specialty(specialtyName, specialtyId);
+        stylistSpecialties.Add(newSpecialty);
+      }
       conn.Close();
       if (conn != null)
       {
-       conn.Dispose();
+        conn.Dispose();
+      }
+      return stylistSpecialties;
+    }
+
+    public void AddSpecialty(Specialty newSpecialty)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand();
+      cmd.CommandText = @"INSERT INTO stylists_specialties (stylist_id, specialty_id) VALUES (@stylist_id, @specialty_id);";
+      MySqlParameter stylistId = new MySqlParameter();
+      stylistId.ParameterName = "@stylist_id";
+      stylistId.Value = _id;
+      cmd.Parameters.Add(stylistId);
+      MySqlParameter specialtyId = new MySqlParameter();
+      specialtyId.ParameterName = "@specialty_id";
+      specialtyId.Value = newSpecialty.GetId();
+      cmd.Parameters.Add(specialtyId);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if(conn!=null)
+      {
+        conn.Dispose();
       }
     }
 
@@ -173,6 +207,24 @@ namespace HairSalon.Models
       if (conn != null)
       {
         conn.Dispose();
+      }
+    }
+
+    public void DeleteClients()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients WHERE stylist_id = @searchId;";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+       conn.Dispose();
       }
     }
 
